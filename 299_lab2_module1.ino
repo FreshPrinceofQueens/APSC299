4b9bc49345e24944cd964442ddbc662748162ecc
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2020-02-01 15:53:49 -0500

    Updated 299_lab2_module1
    Completed Task 3 and Task 4.

--- 299_lab2_module1.ino
+++ 299_lab2_module1.ino
@@ -33,6 +33,13 @@ float dSpd = 0.2;
 
 int initt = 0;
 
+float distance = 0.0;
+float curVeloc = 0.0;
+float curTime = 0.0;
+float preVeloc = 0.0;
+float preTime = 0.0;
+//LSM6DS3Class myIMU;
+
 Servo tilt, pan, gripper;
 
 //wheel 1 is right wheel, wheel 2 is left
@@ -55,20 +62,36 @@ void setup() {
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
-
-  attachInterrupt(0, forInter1, CHANGE);  //when any change occurs on interupt 0 (aka pin 2), forInter() is called
-  attachInterrupt(1, forInter2, CHANGE);  //pin 3 (left wheel = wheel 2)
+//;
+  attachInterrupt(digitalPinToInterrupt(2), forInter1, CHANGE);  //when any change occurs on interupt 0 (aka pin 2), forInter() is called
+  attachInterrupt(digitalPinToInterrupt(3), forInter2, CHANGE);  //pin 3 (left wheel = wheel 2)
+  //;
   interrupts(); //this function enables interupts
-
+//;
   gripper.attach(9); //associates gripper object with pin 9
   tilt.attach(8); //associates tilt object with pin 8
   pan.attach(10); //associates pan object with pin 10
+  //;
 
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
 }
 
 void forInter1() //interrupt for right wheel (wheel 1)
 {
   count++;
+  
+//  float ttrf = (float)ttr;
+//  distance = ((aSpd2 * (ttrf/1000))+(aSpd1*(ttrf/1000))) /2;
+
+//  float rot = count/16;
+//  float totl = 2 * pi * 0.0325 *rot;
+//  float ttr = millis();
+//  sppddd = totl/(ttr/1000);
+  
+
 }
 
 
@@ -173,9 +196,9 @@ int convertToPWM(int value) {
 
 
 
-float actSpeed1() //wheel 1 aka right wheel aka pin 2 aka interrupt 0
+float getSpeed1() //wheel 1 aka right wheel aka pin 2 aka interrupt 0
 {
-  Serial.println("Getting Right Wheel Speed");
+  //Serial.println("Getting Right Wheel Speed");
   int time2;
   int time1;
 
@@ -187,6 +210,7 @@ float actSpeed1() //wheel 1 aka right wheel aka pin 2 aka interrupt 0
     Serial.print("");
   }
 
+  Serial.println("");
   time2 = millis();
   float diff = time2 - time1;
 
@@ -206,9 +230,9 @@ float actSpeed1() //wheel 1 aka right wheel aka pin 2 aka interrupt 0
 
 
 
-float actSpeed2() //wheel 2 aka left wheel aka pin 3 aka interrupt 1
+float getSpeed2() //wheel 2 aka left wheel aka pin 3 aka interrupt 1
 {
-  Serial.println("Getting left wheel speed");
+  //Serial.println("Getting left wheel speed");
   int time2;
   int time1;
 
@@ -220,6 +244,7 @@ float actSpeed2() //wheel 2 aka left wheel aka pin 3 aka interrupt 1
     Serial.print("");
   }
 
+  Serial.println("");
   time2 = millis();
   float diff = time2 - time1;
 
@@ -243,7 +268,7 @@ void adaptToSpeed()
 {
   Serial.println("Adapting");
   adaptwheel1(dSpd);
-  adaptwheel2(dSpd);
+  adaptwheel2(dSpd);//used to be dSpd
   Serial.println("Adapted");
   driveMotor1(wSpd1, wRot1);
   driveMotor2(wSpd2, wRot2);
@@ -256,19 +281,21 @@ void adaptToSpeed()
 void adaptwheel1(float desired)
 {
 
-  aSpd1 = actSpeed1();
-  Serial.println((String)"Wheel 1 PWM BEFORE "+wSpd1);
+  //aSpd1 = getSpeed1();
+  //Serial.println((String)"Wheel 1 PWM BEFORE "+wSpd1);
   
   float diff = desired - aSpd1;
   //
+  int r_wheel_change = 0;
   
-  
-  int r_wheel_change = (int) (K * (diff)); //returns change for pulse width modulations
+  if(diff >= 0.01 || diff <= -0.01) r_wheel_change = (int) (K * (diff)); //returns change for pulse width modulations
+  else r_wheel_change = 0;
   wSpd1 += r_wheel_change;
+  if(wSpd1 >= 255) wSpd1 = 250;
  
   
   
-  Serial.println((String)"Wheel 1 PWM AFTER "+wSpd1);
+  //Serial.println((String)"Wheel 1 PWM AFTER "+wSpd1);
 }
 
 
@@ -277,16 +304,104 @@ void adaptwheel1(float desired)
 
 void adaptwheel2(float desired)
 {
-  aSpd2 = actSpeed2();
-  Serial.println((String)"Wheel 2 PWM BEFORE "+wSpd2);
+  //aSpd2 = getSpeed2();
+  //Serial.println((String)"Wheel 2 PWM BEFORE "+wSpd2);
 
   float diff = desired - aSpd2;
+  int l_wheel_change = 0;
   
-  int l_wheel_change = (int)(K * (diff)); //returns change for pulse width modulations
+  if(diff >= 0.01 || diff <= -0.01) l_wheel_change = (int)(K * (diff)); //returns change for pulse width modulations
+  else l_wheel_change = 0;
   wSpd2 += l_wheel_change;
+  if(wSpd2 >= 255) wSpd2 = 250;
   
   
-  Serial.println((String)"Wheel 2 PWM After "+wSpd2);
+  //Serial.println((String)"Wheel 2 PWM After "+wSpd2);
+}
+
+
+
+
+
+float getIMUAccel(){
+  float accelx = 0;
+  float accely = 0;
+  float accelz = 0;
+
+  if (IMU.accelerationAvailable())
+  {
+    IMU.readAcceleration(accelx, accely, accelz);
+
+    accelx = accelx * 9.81;
+    accely = accely * 9.81;
+
+    float groundAccel = sqrt((accelx*accelx)+(accely*accely));
+
+    if(groundAccel < 0.30) groundAccel = 0;
+
+    return groundAccel;
+  }
+  else return -1;
+}
+
+
+
+
+float getWDistance(){
+  aSpd1 = getSpeed1();
+  aSpd2 = getSpeed2();
+
+  curVeloc = (aSpd1+aSpd2)/2;
+  curTime = millis()/1000;
+  float timediff = curTime - preTime;
+
+  float addeddistance = ((curVeloc + preVeloc)/2) *timediff;
+  distance = distance + addeddistance;
+
+  preTime = curTime;
+  preVeloc = curVeloc;
+  
+  return distance;
+}
+
+
+
+
+float getWAccel()
+{
+  aSpd1 = getSpeed1();
+  aSpd2 = getSpeed2();
+
+  curVeloc = (aSpd1+aSpd2)/2;
+  curTime = millis()/1000;
+  float timediff = curTime - preTime;
+
+  float accelNow = ((curVeloc - preVeloc) / timediff);
+  return accelNow;
+}
+
+
+
+
+void printInfo(){
+  float accel = getWAccel();
+  aSpd1 = getSpeed1();
+  aSpd2 = getSpeed2();
+  float dists = getWDistance();
+
+  Serial.println("Robot Movement Information: ");
+  Serial.print("Right Wheel Velocity: ");
+  Serial.print(aSpd1);
+  Serial.println(" m/s");
+  Serial.print("Left Wheel Velocity: ");
+  Serial.print(aSpd2);
+  Serial.println(" m/s");
+  Serial.print("Current Aceelaration: ");
+  Serial.print(accel);
+  Serial.println(" m/s2");
+  Serial.print("Total Distance Travelled: ");
+  Serial.print(dists);
+  Serial.println(" m");
 }
 
 
@@ -296,32 +411,44 @@ void adaptwheel2(float desired)
 void loop()
 {
 
-
+  Serial.println("----------------------------Sequence Begin----------------------------");
+  wSpd1 = wSpd2 = 128;
+  wRot1 = wRot2 = HIGH;
   driveMotor1(wSpd1, wRot1);
   driveMotor2(wSpd2, wRot2);
 
-  //Serial.println((String)"Count: "+count);
-  //Serial.println((String)"Count2: "+count2);
+  printInfo();
+  delay(1000);
 
+  /*
+  
+  Serial.println("----------------------------Sequence Begin----------------------------");
+  
+  
+  driveMotor1(wSpd1, wRot1);
+  driveMotor2(wSpd2, wRot2);
 
-  aSpd1 = actSpeed1();
-  aSpd2 = actSpeed2();
+//  Serial.println(sppddd);
+  aSpd1 = getSpeed1();
+  aSpd2 = getSpeed2();
   Serial.println("");
   Serial.println((String)"Right wheel is" + aSpd1 + " m/s");
   Serial.println((String)"Left wheel is" + aSpd2 + " m/s");
- // delay(2000);
-
+  Serial.println((String)"Travelled "+distance+" m");
   adaptToSpeed();
 
- // delay(2000);
-
-  aSpd1 = actSpeed1();
-  aSpd2 = actSpeed2();
+  aSpd1 = getSpeed1();
+  aSpd2 = getSpeed2();
   Serial.println("");
   Serial.println((String)"Right wheel is" + aSpd1 + " m/s");
   Serial.println((String)"Left wheel is" + aSpd2 + " m/s");
- // delay(4000);
+ 
+//  float myacc = curAccel();
+//  Serial.print("Current Acceleration is ");
+//  Serial.print(myacc);
+//  Serial.println(" m/s");
 
-
-  Serial.println("Sequence Completed");
+  Serial.println("----------------------------Sequence Completed----------------------------");
+  delay(6000);
+*/
 }
